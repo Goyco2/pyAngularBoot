aca6ca78f632744db7d2c9f283921352590c8693
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2022-11-14 09:56:06 +0000

    1

--- angularBo/src/app/foo/foo.component.ts
+++ angularBo/src/app/foo/foo.component.ts
@@ -1,46 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {Foo} from './foo.model';
 
 @Component({
   selector: 'app-foo',
   templateUrl: './foo.component.html',
   styleUrls: ['./foo.component.css']
 })
-export class FooComponent implements OnInit {
-  data: Object;
-  loading: boolean;
-  o :Observable<Object>;
+export class FooComponent{
+  data!: Object;
+  loading!: boolean;
+  o!: Observable<Object>;
+  fooData !: Foo[];
+  oFoo !: Observable<Foo[]>;
   constructor(public http: HttpClient) {}
-
-  makeCompactRequest(): void {
+   makeRequest(): void {
+    console.log("here");
+    this.loading = true;
+    this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+    this.o.subscribe(this.getData);
+  }
+  getData = (d : Object) =>
+  {
+    this.data = new Object(d);
+    this.loading = false;
+  }
+  
+  makeCompactPost(): void {
     this.loading = true;
     this.http
-      .get('https://jsonplaceholder.typicode.com/posts/1')
-      .subscribe(newData => {
-      this.data = newData;
-      this.loading = false;
+      .post('https://jsonplaceholder.typicode.com/posts',
+        JSON.stringify({
+          body: 'bar',
+          title: 'foo',
+          userId: 1
+        })
+      )
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
       });
-     }
-     makeCompactPost(): void {
-      this.loading = true;
-      this.http
-        .post('https://jsonplaceholder.typicode.com/posts',
-          JSON.stringify({
-            body: 'bar',
-            title: 'foo',
-            userId: 1
-          })
-        )
-        .subscribe(data => {
-          this.data = data;
-          this.loading = false;
-        });
-    }
-   
+  }
+  makeTypedRequest() : void
+ {
+   //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe
+   this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+   this.oFoo.subscribe(data => {this.fooData = data;});
+ }
 
 
   ngOnInit(): void {
   }
+
 
 }
